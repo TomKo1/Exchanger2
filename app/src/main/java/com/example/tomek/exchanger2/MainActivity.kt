1fa7374c7bc7118339f92632ebca18f93e7abfc0
TomKo1 <kottomasz98@gmail.com>
2018-02-24 12:56:09 +0100

    Hamburger displays drawer

--- app/src/main/java/com/example/tomek/exchanger2/MainActivity.kt
+++ app/src/main/java/com/example/tomek/exchanger2/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.tomek.exchanger2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.NonNull
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.Toast
+import android.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // emergency check if the user is logged
         fireBaseAuthe = FirebaseAuth.getInstance()
         if(fireBaseAuthe.currentUser == null){
             finish()
@@ -23,7 +32,11 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        // temporary
+        configDrawer()
+
+
+
+        // temporary code to detect which user is logged
         var currentUsrEmail:String=fireBaseAuthe.currentUser!!.email.toString()
         Toast.makeText(this,"Welcome "+currentUsrEmail,Toast.LENGTH_LONG).show()
 
@@ -33,11 +46,50 @@ class MainActivity : AppCompatActivity() {
         }
 //fireBaseAuthe.signOut()
 
+    private fun configDrawer(){
+        val toggle = ActionBarDrawerToggle(this, drawer_layout,R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val actionBar = supportActionBar
+
+        if(actionBar !=null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeButtonEnabled(true)
+        }
+
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // public boolean onNavigationItemSelected(MenuItem item);
+        nav_view.setNavigationItemSelectedListener {
+            item ->
+                when(item.itemId){
+
+                }
+                drawer_layout.closeDrawer(Gravity.START)
+
+                true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            android.R.id.home -> toggle()
+        }
+        return true
+    }
 
 
 
 
-
+    // detects the state of drawer and closes or opens it
+    private fun toggle(){
+        if(drawer_layout.isDrawerOpen(Gravity.START)){
+            drawer_layout.closeDrawer(Gravity.START)
+        }else{
+            drawer_layout.openDrawer(Gravity.START)
+        }
+    }
 
 
 
